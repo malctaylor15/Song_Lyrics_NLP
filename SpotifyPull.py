1b72b266126342ba439dbe490da45b755df3e7f6
Devin <drose94@gmail.com>
2017-11-20 19:35:37 -0500

    Edits

--- SpotifyPull.py
+++ SpotifyPull.py
@@ -1,7 +1,7 @@
 import os
 from os import environ
-import subprocess
-import sys
+#import subprocess
+#import sys
 from pprint import pprint as pp
 os.environ['SPOTIPY_CLIENT_ID'] = "d3c68e4eb95942fb9a0ceb508d62c127"
 os.environ['SPOTIPY_CLIENT_SECRET'] = "bab6935eaa2f478ea4c47a6c8a96eec8"
@@ -23,26 +23,10 @@ def show_tracks(tracks):
 def get_tracks(username):
     import spotipy
     import spotipy.util as util
-    #from spotipy.oauth2 import SpotifyClientCredentials
-    #import sys
-    
-    '''
-    export SPOTIPY_CLIENT_ID='d3c68e4eb95942fb9a0ceb508d62c127'
-    export SPOTIPY_CLIENT_SECRET='bab6935eaa2f478ea4c47a6c8a96eec8'
-    export SPOTIPY_REDIRECT_URI='http://localhost/' '''
 
-    #client_credentials_manager = SpotifyClientCredentials('d3c68e4eb95942fb9a0ceb508d62c127','bab6935eaa2f478ea4c47a6c8a96eec8')
-    #sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     scope = 'user-library-read playlist-read-private user-top-read'
-    username = '1282829978'
-    
-    
+    #username = '1282829978'
 
-    '''if len(sys.argv) > 1:
-        username = sys.argv[1]
-    else:
-        print("Usage: %s username" % (sys.argv[0],))
-        sys.exit()'''
     token = util.prompt_for_user_token(username,scope)#client_id='d3c68e4eb95942fb9a0ceb508d62c127',client_secret='bab6935eaa2f478ea4c47a6c8a96eec8',redirect_uri='http://localhost/')
     
     if token:
