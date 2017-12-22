0e6e88065249c774221b63bef3dd7b829beab761
malctaylor15 <malctaylor15@gmail.com>
2017-12-22 01:42:19 -0500

    Begin support for word vectors and aggregations
    Added function to playlist class to aggregate song sentiments into list
    Began looking into word vectors. Current implementation is broken

--- Spotify+Pulls.py
+++ Spotify+Pulls.py
@@ -3,30 +3,33 @@
 
 # In[ ]:
 
+# System utilities 
 import os
 from os import environ
 import subprocess
 import sys
 
-from pprint import pprint as pp
+# Spotify API 
 import spotipy
 import spotipy.util as util
+
+# NLP Libraries 
 from wordcloud import WordCloud
 from textblob import TextBlob
+import gensim.models.word2vec as w2v
+from nltk.tokenize import sent_tokenize, word_tokenize
+import gensim
+
+
+# Random 
+import re 
+from pprint import pprint as pp
 
 # Set environment variables 
 os.environ['SPOTIPY_CLIENT_ID'] = "d3c68e4eb95942fb9a0ceb508d62c127"
 os.environ['SPOTIPY_CLIENT_SECRET'] = "bab6935eaa2f478ea4c47a6c8a96eec8"
 os.environ['SPOTIPY_REDIRECT_URI'] = "http://localhost/"
 
-
-# In[ ]:
-
-import gensim
-import re 
-
-    
-# ## Functions
 
 # In[ ]:
 
@@ -99,8 +102,9 @@ playlists = sp.user_playlists(username)
 playlists_info = get_user_playlists_2(username)
 pp(playlists_info)
 # In[ ]:
-playlist_index = 2
-tracklist = get_tracklist(playlists_info[playlist_index][2], username)
+playlist_index = 1
+spotify_tracklist_id = playlists_info[playlist_index][2]
+tracklist = get_tracklist(spotify_tracklist_id, username)
 
 # In[ ]:
 
@@ -109,21 +113,16 @@ tracklist = get_tracklist(playlists_info[playlist_index][2], username)
 currentDirectory = os.getcwd()
 sys.path.append(currentDirectory)
 from GeniusAPI_MT import *
-#from GeniusAPI_MT import *
 
 
 # In[ ]:
-
+"""
 lyrics_dict = {}
 for song_name, artist in tracklist.items():
     lyrics_dict[song_name] = get_song_lyrics(artist_name=artist, song_title=song_name, headers=headers)
 
-
-# In[ ]:
-
-lyrics_dict.keys()
-
-
+print(lyrics_dict.keys())
+"""
 # In[ ]:
 
 class song: 
@@ -132,14 +131,20 @@ class song:
         self.artist = artist
         self.lyrics = get_song_lyrics(self.artist, self.title, headers = headers)
         self.blob = TextBlob(self.lyrics) #initialize the object for TextBlob
+        
     def showWordCloud(self):
         if self.lyrics == ():
             print("No lyrics found")
             return ()
         self.wc = WordCloud().generate(self.lyrics)
         self.wc.to_image().show()
-    def getSentiment(self):
+        
+    def getSentiment(self, trace = 0):
         self.polarity = self.blob.sentiment.polarity
+        if trace:
+            print("Polarity is ", self.polarity)
+        return(self.polarity)
+        
     def getTags(self):
         print(self.blob.tags)
     def getNouns(self):
@@ -151,14 +156,30 @@ class playlist:
     def __init__(self, tracklist_id, username):
         self.username = username
         self.tracklist_id = tracklist_id
-        self.tracklist = get_tracklist_class(self.tracklist_id, self.username)
-        self.allLyrics = get_tracklist_lyrics(self.tracklist)
+        self.listOfSongs = get_tracklist_class(self.tracklist_id, self.username)
+        self.allLyrics = get_tracklist_lyrics(self.listOfSongs)
+        
     def showWordCloud(self):
         self.wc = WordCloud().generate(self.allLyrics)
         self.wc.to_image().show()
+    
+    def getSentimentList(self):
+        self.sentimentList = [song.getSentiment() for song in self.listOfSongs]
+        return(self.sentimentList)
         
-
+    def buildWord2Vec(self, num_features = 50, context_size = 7, min_word_count = 1):
+        corpus = [song.lyrics for song in self.listOfSongs]
+        downsampling = 1e-1
         
+        songs2vec = w2v.Word2Vec(
+            sg=1,
+            seed=1234,
+            size=num_features,
+            min_count=min_word_count,
+            window=context_size,
+            sample=downsampling
+            )
+        songs2vec.build_vocab(corpus)
     
 
 
@@ -167,18 +188,78 @@ class playlist:
 ### Testing the object functionality
 testSong = song('Rap God', 'Eminem')
 testSong.getSentiment()
-testSong.getTags()
-testSong.getNouns()
-print(testSong.polarity)
-pp(testSong.lyrics)
 
 # In[ ]:
 
 testSong.showWordCloud()
+print(testSong.polarity)
+
+# In[ ]: 
+
+testPlaylist = playlist(spotify_tracklist_id, username)
+
+# In[ ]: 
+
+songlist = testPlaylist.listOfSongs
+corpus = []
+wc_corpus = str()
+for song in songlist:
+    words = song.lyrics.lower().split()
+    for word in words:
+        corpus.append(words)
+        wc_corpus = wc_corpus + ' ' + word
+# In [9]:
+print(len(corpus))
+print(len(wc_corpus))
+
+# In[ ]:
+songs2vec = w2v.Word2Vec(
+    sg=1,
+    seed=1234,
+    size=20,
+    min_count=1,
+    window=7,
+    sample=1e-1
+    )
+
+songs2vec.build_vocab(corpus)
+songs2vec.corpus_count
+songs2vec.train(corpus, total_examples = songs2vec.corpus_count
+                , epochs = songs2vec.iter)
+
+songs2vec.most_similar('vibe')
+
+wc1 = WordCloud().generate(wc_corpus) 
+wc1.to_image().show()
+
+
+# In[ ]:
+def wordListToFreqDict(wordlist):
+    wordfreq = [wordlist.count(p) for p in wordlist]
+    return dict(zip(wordlist,wordfreq))
+
+
+# In[ ]:
+wordListToFreqDict(corpus[1])
+# In[ ]:
+
+# In[ ]:
+
+
 
 # In[ ]:
     
 """Playlist1 = playlist(playlists_info[playlist_index][2], username)
+    
+# Helpful code from Kaggle
+#articles_tokens.append([word for word in word_tokenize(str(df["text"][i].lower())) if len(word)>2])
+model = gensim.models.Word2Vec(articles_tokens, min_count=5,size=100,workers=4)
+model.wv.most_similar("lula")
+model.wv.most_similar("propina")
+
+
+In [10]:
+
 
 # In[ ]:
 Playlist1.showWordCloud()
