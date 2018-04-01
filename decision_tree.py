9023147806ddbd860dab03d6fcda55f231e15e4d
Devin <drose94@gmail.com>
2018-04-01 12:23:33 -0400

    slight edits to scitkit-learn method

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,9 @@
 ### Sci-kit documentation ###
 from sklearn import tree
-X = [[0,0], [1,1]]
+X = [
+     [0,0],
+     [1,1]
+     ]
 Y = [0, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
@@ -24,13 +27,23 @@ print("Shape: ", balance_data.shape)
 balance_data.head()
 #
 
-## Example of portion of train_test_split() method
 X = balance_data.values[:, 1:5]
 Y = balance_data.values[:, 0]
-##
-
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 100)
+
+clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100,
+                               max_depth=3, min_samples_leaf=5)
+clf_gini.fit(X_train, y_train)
+
+clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100,
+ max_depth=3, min_samples_leaf=5)
+clf_entropy.fit(X_train, y_train)
+
+clf_gini.predict([[4, 4, 3, 3]])
+y_pred = clf_gini.predict(X_test)
+y_pred
+
 
 ############################################
 ### Visual map ###
